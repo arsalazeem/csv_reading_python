c264944f03d5b19da9b3dba94874ed43f112e24b
arsalazeem24 <azeemarsal@gmail.com>
2023-06-01 12:01:35 +0500

    generic class to read csv data or to dump it into a json file

--- main.py
+++ main.py
@@ -1,79 +1,56 @@
-import openpyxl
-import json
 import csv
-from csv import reader as rd
-#code for reading a xlsx file
-# book = openpyxl.load_workbook('file_1.xlsx')
-# sheet = book.active
-# k=0
-# for i in range(1,some_range+1):
-#     a1 = sheet.cell(row=i, column=1)
-#     if type(a1.value)==int:
-#         sellerId=sheet.cell(row=i,column=1)
-#         sellerId=sellerId.value
-#         email = sheet.cell(row=i, column=2)
-#         email= email.value
-#         serviceId= sheet.cell(row=i, column=3)
-#         serviceId = serviceId.value
-#         serviceName= sheet.cell(row=i, column=3)
-#         serviceName = serviceName.value
-#         description = sheet.cell(row=i, column=3)
-#         description = description.value
-#         isFeatured = sheet.cell(row=i, column=3)
-#         isFeatured = isFeatured.value
-#         categoryName = sheet.cell(row=i, column=3)
-#         categoryName= categoryName.value
-
-count = 0
-master_list = []
-
-with open('second_file.csv', 'r', encoding='cp1252') as file:
-    reader = csv.reader(file)
-    try:
-
-        for row in reader:
-            print(row)
-            count = count + 1
-            try:
-                current_id = int(row[0])
-                selleId = row[0]
-                firstName = row[1]
-                lastName = row[2]
-                email = row[3]
-                dialCode = row[4]
-                phoneNumber = row[5]
-                aboutMe = row[6]
-                sellerType = row[7]
-                agencyName = row[8]
-                title = row[9]
-                streetAddress = row[10]
-                apartment = row[11]
-                city = row[12]
-                state = row[13]
-                master_list.append({
-                    "selleId": current_id,
-                    "firstName": firstName,
-                    "lastName": lastName,
-                    "email": email,
-                    "dialCode": dialCode,
-                    "phoneNumber": phoneNumber,
-                    "aboutMe": aboutMe,
-                    "sellerType": sellerType,
-                    "agencyName": agencyName,
-                    "title": title,
-                    "streetAddress": streetAddress,
-                    "apartment": apartment,
-                    "city": city,
-                    "state": state})
-
-            except:
-                pass
-    except:
-        pass
+import json
+import chardet
+import io
 
 
-jsonString = json.dumps(master_list)
-jsonFile = open("activeSellers.json", "w")
-jsonFile.write(jsonString)
-jsonFile.close()
 
+class CSVReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    @staticmethod
+    def show_data(data, data_type=None):
+        if data_type is None:
+            print(data)
+        elif data_type == 'json':
+            print(json.dumps(data, indent=4))
+
+    def read_data(self):
+        data = []
+        try:
+            with open(self.file_path, 'rb') as csv_file:
+                raw_data = csv_file.read()
+                result = chardet.detect(raw_data)
+                encoding = result['encoding']
+                csv_file.seek(0)
+                reader = csv.reader(io.TextIOWrapper(csv_file, encoding=encoding))
+                for row in reader:
+                    data.append(row)
+        except FileNotFoundError:
+            print("File not found.")
+        except Exception as e:
+            print("An error occurred:", str(e))
+        self.show_data(data)
+        return data
+
+    @staticmethod
+    def dump_json_data(data, filename):
+        with open(f'{filename}.json', 'w') as infile:
+            json.dump(data, infile,indent=4)
+
+    def read_data_as_json_string(self):
+        self.show_data(self.read_data(), 'json')
+        return json.dumps(self.read_data(), indent=4)
+
+    def read_csv_data_and_dump_as_json_file(self, filename):
+        if '.json' in filename:
+            filename = filename.replace('json', '')
+        self.dump_json_data(self.read_data(), filename)
+
+
+file_path = "csv_files/Sample-Spreadsheet-100-rows.csv"
+reader = CSVReader(file_path)
+reader.read_data()
+reader.read_data_as_json_string()
+reader.read_csv_data_and_dump_as_json_file(file_path)
